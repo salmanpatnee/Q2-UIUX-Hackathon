459380c004466c7bef93d2a21a6c7a43051e18fb
salmanpatnee <salmanpatni92@gmail.com>
2024-12-08 18:48:47 +0500

    Add mini cart widget

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,11 +4,16 @@ import Link from "next/link";
 import Navbar from "./components/Navbar";
 import { Heart, Search, ShoppingCart, UserRound } from "lucide-react";
 import MobileNavbar from "./components/MobileNavbar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 const Header = () => {
   return (
     <header className="border border-b py-8">
       <div className="wrapper flex items-center justify-between ">
-       
         <div className="col logo w-full text-center md:w-auto">
           <Link href="/">
             <Image
@@ -25,10 +30,9 @@ const Header = () => {
         <div className="col menu hidden lg:block">
           <Navbar />
         </div>
-         <div className="lg:hidden w-2/12 text-end">
+        <div className="lg:hidden w-2/12 text-end">
           <MobileNavbar />
         </div>
-        
 
         <div className="col action hidden lg:block">
           <div className="flex space-x-11 items-center justify-end">
@@ -50,15 +54,67 @@ const Header = () => {
                 className="stroke-current text-black  transition-colors group-hover:stroke-primary"
               />
             </Link>
-            <Link href="/cart" className="group">
+            <Popover>
+              <PopoverTrigger className="group">
+                <ShoppingCart
+                  size={32}
+                  className="stroke-current text-black  transition-colors group-hover:stroke-primary"
+                />
+              </PopoverTrigger>
+              <PopoverContent className="rounded-none white w-96 py-7 px-8">
+                <h4 className="font-semibold text-xl border-b border-[#D9D9D9] pb-7 mb-10">Shopping Cart</h4>
+                <div className="flex items-center justify-between mb-5">
+                  <div>
+                    <Image src="/images/cart-item-1.png" alt="sofa" width={111} height={90} className="rounded-lg bg-[#efe6d1]"/>
+                  </div>
+                  <div>
+                    <p className="text-base mb-2">Asgaard sofa</p>
+                    <p className="flex space-x-4 items-center">
+                      <span className="font-light text-base">1</span>
+                      <span className="font-light text-base">X</span>
+                      <span className="font-medium text-primary text-xs">Rs. 250,000.00 </span>
+                    </p>
+                  </div>
+                  <div>
+                    <Image src="/images/icons/close.png" width={20} height={20} alt="close" className="cursor-pointer"/>
+                  </div>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <Image src="/images/cart-item-1.png" alt="sofa" width={111} height={90} className="rounded-lg bg-[#efe6d1]"/>
+                  </div>
+                  <div>
+                    <p className="text-base mb-2">Asgaard sofa</p>
+                    <p className="flex space-x-4 items-center">
+                      <span className="font-light text-base">1</span>
+                      <span className="font-light text-base">X</span>
+                      <span className="font-medium text-primary text-xs">Rs. 250,000.00 </span>
+                    </p>
+                  </div>
+                  <div>
+                    <Image src="/images/icons/close.png" width={20} height={20} alt="close" className="cursor-pointer"/>
+                  </div>
+                </div>
+                <div  className="mt-20 flex text-base  justify-between border-b border-[#D9D9D9] pb-6">
+                  <p>Subtotal</p>
+                  <p className="text-primary font-medium">Rs. 520,000.00</p>
+                </div>
+                <div className="flex items-center justify-between pt-7 space-x-2">
+                <Link href="/cart" className="border rounded-2xl text-xs py-2 px-6 hover:bg-primary hover:text-white transition-colors">Cart</Link>
+                <Link href="/cart" className="border rounded-2xl text-xs py-2 px-6 hover:bg-primary hover:text-white transition-colors">Checkout</Link>
+                <Link href="/cart" className="border rounded-2xl text-xs py-2 px-6 hover:bg-primary hover:text-white transition-colors">Comparision</Link>
+                </div>
+              </PopoverContent>
+            </Popover>
+
+            {/* <Link href="/cart" className="group">
               <ShoppingCart
                 size={32}
                 className="stroke-current text-black  transition-colors group-hover:stroke-primary"
               />
-            </Link>
+            </Link> */}
           </div>
         </div>
-        
       </div>
     </header>
   );
